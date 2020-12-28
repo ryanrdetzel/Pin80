f1780847c4320b8140bc2c8655deb75c7da0ab61
Ryan Detzel <ryandetzel@gmail.com>
2020-12-28 12:14:02 -0500

    VPX Processor updates

--- Pin80Server/CommandProcessors/VPXProcessor.cs
+++ Pin80Server/CommandProcessors/VPXProcessor.cs
@@ -9,6 +9,7 @@ namespace Pin80Server.CommandProcessors
     {
         private SerialPort serial;
         private string _romName;
+        private const int LagIgnoreMS = 30;
 
         public VPXProcessor(SerialPort s)
         {
@@ -30,49 +31,62 @@ namespace Pin80Server.CommandProcessors
                 return false;
             }
 
-            string[] commandParts = command.Split(' ');
-            string trigger = commandParts[0];
-            string value = commandParts[1];
-            string extra = string.Join(" ", commandParts.Skip(2));
-
-            var now = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-
-            /* Table information */
-            if (trigger == "INFO")
-            {
-                if (value == "ROM")
-                {
-                    _romName = extra;
-                    callback(this);
-                    return true;
-                }
-            }
-
-
-            if (!trigger.StartsWith("L")) //Lamps
-            {
-                return true;
-            }
-
-            //Debug.WriteLine("Process... " + command);
             try
             {
+                string[] commandParts = command.Split(' ');
+
+                if (commandParts.Length < 3)
+                {
+                    Debug.WriteLine(String.Format("Command is not valid: {0}", command));
+                    return false;
+                }
+
+                string trigger = commandParts[0];
+                string value = commandParts[1];
+                string extra = string.Join(" ", commandParts.Skip(2));
+
+                var now = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+                /* Table information */
+                if (trigger == "INFO")
+                {
+                    if (value == "ROM")
+                    {
+                        _romName = extra;
+                        callback(this);
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                if (trigger.StartsWith("L")) //Lamps
+                {
+                    return true;
+                }
+
+
                 var sentMS = Convert.ToInt64(extra);
                 var lag = now - sentMS;
+
+                if (lag > LagIgnoreMS)
+                {
+                    Debug.WriteLine(String.Format("Ignoring command {0} as it's too old: {1}", command, lag));
+                }
 
                 if (commandParts[0] == "S11" || commandParts[0] == "S13" || commandParts[0] == "S12" || commandParts[0] == "S48" || commandParts[0] == "S46")
                 {
                     // TODO FIX THIS
                     serial.Write(string.Format("{0} {1}\n", "S48", commandParts[1]));
                 }
+
+                return true;
             }
             catch (Exception e)
             {
                 Debug.WriteLine(string.Format("Could not process command: {0} {1}", command, e));
                 return false;
             }
-
-            return true;
         }
     }
 }
