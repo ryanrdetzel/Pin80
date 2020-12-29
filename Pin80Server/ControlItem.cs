8df355b0629bd365b5409b9a769ccdc1ccef18f9
Ryan Detzel <ryandetzel@gmail.com>
2020-12-28 19:40:14 -0500

    New Files

---
+++ Pin80Server/ControlItem.cs
@@ -0,0 +1,24 @@
+﻿
+using Newtonsoft.Json;
+/**
+* Control Item
+*/
+namespace Pin80Server
+{
+    public class ControlItem
+    {
+        [JsonProperty("enabled")] 
+        public  bool enabled { get; set; }
+        [JsonProperty("trigger")]
+        public string trigger { get; set; }
+        [JsonProperty("action")]
+        public string action { get; set; }
+        [JsonProperty("comment")]
+        public string comment { get; set; }
+
+        override public string ToString()
+        {
+            return string.Format("{0} {1}", trigger, action);
+        }
+    }
+}
---
+++ Pin80Server/DataProcessor.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Pin80Server
+{
+    public class DataProcessor
+    {
+        private List<ControlItem> data = new List<ControlItem>();
+
+        public DataProcessor()
+        {
+            data = LoadFile();
+        }
+
+        public List<ControlItem> getProcessedData()
+        {
+            return data;
+        }
+
+        private List<ControlItem> LoadFile()
+        {
+            using (StreamReader r = new StreamReader("example.json"))
+            {
+                string json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<ControlItem>>(json);
+            }
+        }
+    }
+}
